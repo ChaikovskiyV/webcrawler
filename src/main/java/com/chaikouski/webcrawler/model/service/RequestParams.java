b81f4d060608f126bc8039a9ce8e6e111795ca32
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-16 00:23:12 +0300

    RequestParams init.

---
+++ src/main/java/com/chaikouski/webcrawler/model/service/RequestParams.java
@@ -0,0 +1,9 @@
+package com.chaikouski.webcrawler.model.service;
+
+public final class RequestParams {
+    public static final String SEARCH = "search";
+    public static final String LIMIT = "limit";
+
+    private RequestParams() {
+    }
+}
