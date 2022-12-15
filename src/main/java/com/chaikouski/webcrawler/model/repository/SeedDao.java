b6721475ad255d085767a54091ed07a4a4aa7265
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-15 23:53:09 +0300

    SeedDao interface init.

---
+++ src/main/java/com/chaikouski/webcrawler/model/repository/SeedDao.java
@@ -0,0 +1,17 @@
+package com.chaikouski.webcrawler.model.repository;
+
+import com.chaikouski.webcrawler.model.entity.Seed;
+
+import java.util.List;
+
+public interface SeedDao {
+    Seed addSeed(Seed seed);
+
+    List<Seed> getAllSeeds();
+
+    List<Seed> getSeedsBySearchParam(String search);
+
+    List<Seed> getSeedsWithLimitedResult(int limit);
+
+    List<Seed> getSeedsBySearchParamWithLimitedResult(String search, int limit);
+}
