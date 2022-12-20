8d92c8054d21e7aca93dd10a171078c8497d666c
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-21 00:23:37 +0300

    Optimized queries, added new query.

--- src/main/java/com/chaikouski/webcrawler/model/repository/QueriesStorage.java
+++ src/main/java/com/chaikouski/webcrawler/model/repository/QueriesStorage.java
@@ -2,8 +2,9 @@ package com.chaikouski.webcrawler.model.repository;
 
 public final class QueriesStorage {
     public static final String GET_ALL_SEEDS_QUERY = "FROM Seed";
-    public static final String BY_SEARCH_PARAM = " s JOIN Term t WHERE s.url LIKE ?1 OR t.termName LIKE ?1";
-
+    public static final String BY_SEARCH_PARAM = " s JOIN FETCH s.terms t WHERE CONCAT_WS(' ', s.url, t.termName) LIKE ?1";
+    public static final String BY_URL = " s WHERE s.url=?1";
+    public static final String GET_TERM_BY_NAME_AND_REPETITION = "FROM Term t WHERE t.termName=?1 AND t.repetitionsNumber=?2";
 
     private QueriesStorage() {
     }
