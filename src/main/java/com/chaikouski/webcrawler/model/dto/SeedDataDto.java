dc86e919a7aa57030317cb40b0be53cdf724054a
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-25 14:07:13 +0300

    Overridden equals and hashCode methods, replaced space delimiter with empty delimiter on the line 9.

--- src/main/java/com/chaikouski/webcrawler/model/dto/SeedDataDto.java
+++ src/main/java/com/chaikouski/webcrawler/model/dto/SeedDataDto.java
@@ -1,14 +1,29 @@
 package com.chaikouski.webcrawler.model.dto;
 
+import java.util.Objects;
+
 public class SeedDataDto {
     private final String seedData;
 
     public SeedDataDto(String url, String termRepetitions) {
-        seedData = String.join(" ", url, termRepetitions);
+        seedData = String.join("", url, termRepetitions);
     }
 
     public String getSeedData() {
         return seedData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SeedDataDto that = (SeedDataDto) o;
+        return Objects.equals(seedData, that.seedData);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seedData);
     }
 
     @Override
