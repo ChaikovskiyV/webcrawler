b91a9dafa31d0e99cc973d8b9391276c5148227c
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-25 14:17:28 +0300

    Added explanations and addition test.

--- src/test/java/com/chaikouski/webcrawler/model/util/CsvWriterTest.java
+++ src/test/java/com/chaikouski/webcrawler/model/util/CsvWriterTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,35 +14,66 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * @author Viktar Chaikouski
+ * <p>
+ * The type CsvWriterTest.
+ * <p>
+ * This class includes tests for methods of the CsvWriter class.
+ */
+@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestPropertySource("classpath:application-test.properties")
 class CsvWriterTest {
+    @Autowired
     private CsvWriter writer;
     private List<String> data;
     private Path filePath;
+    @Value("${csv.file-name}")
+    private String fileName;
 
+    /**
+     * Inits test variables and test data.
+     */
     @BeforeAll
     void init() {
-        writer = new CsvWriter();
-    }
-
-    @BeforeEach
-    void setUp() throws IOException {
-        filePath = Path.of("test-data-file");
-
-        if(Files.exists(filePath)) {
-            Files.delete(filePath);
-        }
-
+        filePath = Path.of(fileName);
         data = List.of("There", "are", "several", "strings", "in", "this", "list");
     }
 
+    /**
+     * Removes a file with result if it exists.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @BeforeEach
+    void removeFileResultIfExist() throws IOException {
+        if (Files.exists(filePath)) {
+            Files.delete(filePath);
+        }
+    }
+
+    /**
+     * Tests the writeDataToScvFile method whether it creates a file and writes data.
+     *
+     * @throws IOException if an I/O error occurs reading from the file
+     */
     @Test
     void writeDataToScvFileTest() throws IOException {
         writer.writeDataToCsvFile(data);
         List<String> lines = Files.readAllLines(filePath);
 
         assertArrayEquals(data.toArray(), lines.toArray());
+    }
+
+    /**
+     * Tests the writeDataToScvFile method whether it creates a file.
+     */
+    @Test
+    void whetherFileCreatedTest() {
+        writer.writeDataToCsvFile(data);
+
+        assertTrue(Files.exists(filePath));
     }
 }
