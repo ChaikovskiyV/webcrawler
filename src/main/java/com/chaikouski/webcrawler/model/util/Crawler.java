fca3e4e4385aa16ebc49f62ad21331b485637782
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-12-25 13:58:16 +0300

    Added explanations.

--- src/main/java/com/chaikouski/webcrawler/model/util/Crawler.java
+++ src/main/java/com/chaikouski/webcrawler/model/util/Crawler.java
@@ -10,6 +10,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Viktar Chaikouski
+ * <p>
+ * The type Crawler.
+ * <p>
+ * This class is destined to crawl web resources and gather documents.
+ */
 @Component
 public class Crawler {
     private static final int MAX_DEEP_LEVEL = 8;
@@ -18,12 +25,19 @@ public class Crawler {
     private static final String HREF_KEY = "href";
     private static final String HTTP = "http";
 
+    /**
+     * Crawls the web resource with provided url and gathers documents.
+     *
+     * @param url the url of web resource
+     * @return the list of Document
+     */
     public List<Document> crawl(String url) {
         List<Document> documents = new ArrayList<>();
 
         return scan(1, url, documents);
     }
 
+    /*Scans document to find urls of other web resources*/
     private List<Document> scan(int level, String url, List<Document> documents) {
         if (documents.size() < MAX_NUM_VISITED_PAGES && level <= MAX_DEEP_LEVEL) {
             Document document = request(url, documents);
@@ -38,6 +52,8 @@ public class Crawler {
         return documents;
     }
 
+    /*Returns Document of the web resource with provided url and null
+    if the response from this resource differ from 200 or such a document is already exist in the document list.*/
     private Document request(String url, List<Document> documents) {
         Document document = null;
         if (url.startsWith(HTTP) && documents.stream().noneMatch(doc -> doc.baseUri().equals(url))) {
